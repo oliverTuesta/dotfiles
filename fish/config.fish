7db765f99537888bcb4697238946a7b78d122027
Oliver Tuesta <oliver.jtuesta@gmail.com>
2024-09-20 00:42:22 -0500

    fix: change tsserver for ts_ls

--- fish/config.fish
+++ fish/config.fish
@@ -2,3 +2,17 @@ set fish_greeting
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+if test -f /home/spigi/miniconda3/bin/conda
+    eval /home/spigi/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+else
+    if test -f "/home/spigi/miniconda3/etc/fish/conf.d/conda.fish"
+        . "/home/spigi/miniconda3/etc/fish/conf.d/conda.fish"
+    else
+        set -x PATH "/home/spigi/miniconda3/bin" $PATH
+    end
+end
+# <<< conda initialize <<<
+
