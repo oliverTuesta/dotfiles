9709ae8d655689df7c033752a7c59237a840ebd8
spigi <oliver.jtuesta@gmail.com>
2026-01-26 14:51:55 -0500

    feat: add bun to fish

--- fish/config.fish
+++ fish/config.fish
@@ -6,3 +6,7 @@ fnm env --use-on-cd | source
 # Generated for envman. Do not edit.
 test -s ~/.config/envman/load.fish; and source ~/.config/envman/load.fish
 export PATH="$HOME/.local/bin:$PATH"
+
+# bun
+set --export BUN_INSTALL "$HOME/.bun"
+set --export PATH $BUN_INSTALL/bin $PATH
