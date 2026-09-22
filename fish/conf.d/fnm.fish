00a3e7930352e18b032156ce8ecf153ac6800c23
oliver tuesta <oliver.jtuesta@gmail.com>
2026-09-22 12:35:06 -0500

    feat: fish paths

--- fish/conf.d/fnm.fish
+++ fish/conf.d/fnm.fish
@@ -5,3 +5,17 @@ if [ -d "$FNM_PATH" ]
   set PATH "$FNM_PATH" $PATH
   fnm env | source
 end
+
+# fnm
+set FNM_PATH "/home/spigi/.local/share/fnm"
+if [ -d "$FNM_PATH" ]
+  set PATH "$FNM_PATH" $PATH
+  fnm env --shell fish | source
+end
+
+# fnm
+set FNM_PATH "/home/spigi/.local/share/fnm"
+if [ -d "$FNM_PATH" ]
+  set PATH "$FNM_PATH" $PATH
+  fnm env --shell fish | source
+end
